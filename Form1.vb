fad08fbbe7ae34c1e017f8b8fac44a49fc0f0545
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-07 03:04:27 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles Languagename.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim NameofLanguage As String
+
+        NameofLanguage = Languagename.Text
+
+
+
+        If NameofLanguage = "Zulu" Then
+
+            MsgBox("Sawubona")
+
+        ElseIf NameofLanguage = "Xhosa" Then
+
+            MsgBox("Molweni")
+
+        ElseIf NameofLanguage = "Sotho" Then
+            MsgBox("Dumelang")
+
+        Else
+            MsgBox("hi")
+
+
+        End If
+
+
+    End Sub
+End Class
